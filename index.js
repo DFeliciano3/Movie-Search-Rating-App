96c3790cbe244ae4c8f78eba9cc9bcaed5f03b8d
DFeliciano3 <91075867+DFeliciano3@users.noreply.github.com>
2022-07-06 12:16:42 -0400

    Add event listener for search box

--- index.js
+++ index.js
@@ -1,21 +1,39 @@
-const API_KEY = "api_key=84c791a4a4576eee38aa7eeffbb5bccd"; 
-
-const BASE_URL = 'https://api.themoviedb.org/3'
-
-const API_URL= BASE_URL + 'discover/movie?sort_by=popularity.desc'
-+API_KEY;
-
+const myAPIKey = 'api_key=84c791a4a4576eee38aa7eeffbb5bccd'; 
+const domainURL = 'https://api.themoviedb.org/3';
+const pathForAPI = domainURL + 'discover/movie?sort_by=popularity.desc&'+myAPIKey;
 const imageURL = 'https://image.tmdb.org/t/p/w500/';
-
+const searchURL = domainURL + '/search/movie?'+myAPIKey;
 const main = document.getElementById('main');
+const movies = document.getElementsByClassName('movie');
+const form = document.getElementsByTagName('form');
+const search = document.getElementById('search');
+getAllMovies(pathForAPI);
 
-getAllMovies(API_URL)  
+
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+  }
+  
+window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+      let dropdowns = document.getElementsByClassName("dropdown-content");
+      let i;
+      for (i = 0; i < dropdowns.length; i++) {
+        let openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+        }
+      }
+    }
+  }
+
+// getAllMovies(pathForAPI)  
 
 function getAllMovies() {
     fetch('https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=84c791a4a4576eee38aa7eeffbb5bccd')
     .then(res => res.json())
     .then(data => {
-        console.log(data)
+        console.log(data.results)
         showAllMovies(data.results);
     })
 }
@@ -44,3 +62,9 @@ function showAllMovies(data) {
        document.querySelector('main').appendChild(movieEl);
     })
 }
+
+// form.addEventListener('sumbit', (e) => {
+//     e.preventDefault();
+
+//     let searchMovie = search
+// })
