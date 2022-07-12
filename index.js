f4dcbf7ab1281bcc31a5a18c2b4c5835109d6c85
DFeliciano3 <91075867+DFeliciano3@users.noreply.github.com>
2022-07-12 13:47:42 -0400

    Search event listener now functions

--- index.js
+++ index.js
@@ -1,50 +1,40 @@
 const myAPIKey = 'api_key=84c791a4a4576eee38aa7eeffbb5bccd'; 
 const domainURL = 'https://api.themoviedb.org/3';
-const pathForAPI = domainURL + 'discover/movie?sort_by=popularity.desc&'+myAPIKey;
+const pathForAPI = 'https://api.themoviedb.org/3discover/movie?sort_by=popularity.desc&api_key=84c791a4a4576eee38aa7eeffbb5bccd';
 const imageURL = 'https://image.tmdb.org/t/p/w500/';
-const searchURL = domainURL + '/search/movie?'+myAPIKey;
+const searchURL = 'https://api.themoviedb.org/3/search/movie?api_key=84c791a4a4576eee38aa7eeffbb5bccd';
 const main = document.getElementById('main');
-const movies = document.getElementsByClassName('movie');
-const form = document.getElementsByTagName('form');
-const search = document.getElementById('search');
-getAllMovies(pathForAPI);
 
+document.addEventListener('DOMContentLoaded', () => {
+    document.querySelector('form').addEventListener('submit', (e) => {
+        e.preventDefault()
+        let inputSearch = e.target.search.value
 
-function myFunction() {
-    document.getElementById("myDropdown").classList.toggle("show");
-  }
+        fetch(searchURL+'&query='+inputSearch)
+        .then(res => res.json())
+        .then(data => showAllMovies(data.results))
+    })
+})
   
-window.onclick = function(event) {
-    if (!event.target.matches('.dropbtn')) {
-      let dropdowns = document.getElementsByClassName("dropdown-content");
-      let i;
-      for (i = 0; i < dropdowns.length; i++) {
-        let openDropdown = dropdowns[i];
-        if (openDropdown.classList.contains('show')) {
-          openDropdown.classList.remove('show');
-        }
-      }
-    }
-  }
-
-// getAllMovies(pathForAPI)  
 
 function getAllMovies() {
     fetch('https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=84c791a4a4576eee38aa7eeffbb5bccd')
     .then(res => res.json())
     .then(data => {
-        console.log(data.results)
+       // console.log(data.results)
         showAllMovies(data.results);
     })
 }
 
 
+
 function showAllMovies(data) {
      document.querySelector('main').innerHTML = '';
 
-    data.forEach(movie => {
-        const {title, poster_path, vote_average, overview} = movie;
-        const movieEl = document.createElement('div');
+        data.forEach(movie => {
+        let {title, poster_path, vote_average, overview} = movie;
+        let movieEl = document.createElement('div');
+
         movieEl.classList.add('movie');
         movieEl.innerHTML =  `
         <img src="${imageURL+poster_path}" alt="${title}">
@@ -61,10 +51,6 @@ function showAllMovies(data) {
 
        document.querySelector('main').appendChild(movieEl);
     })
-}
+};
 
-// form.addEventListener('sumbit', (e) => {
-//     e.preventDefault();
-
-//     let searchMovie = search
-// })
+getAllMovies(pathForAPI)
